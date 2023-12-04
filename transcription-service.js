c57cf9203d345b477f5937bbb5148e0cbdbca222
Charlie Weems <charlie.weems@gmail.com>
2023-12-04 09:32:49 -0800

    Use Deepgram endpointing to determine when user has stopped speaking

--- transcription-service.js
+++ transcription-service.js
@@ -9,20 +9,25 @@ class TranscriptionService extends EventEmitter {
     this.deepgramLive = deepgram.transcription.live({
       encoding: "mulaw",
       sample_rate: "8000",
-      model: "nova",
+      model: "nova-2",
       punctuate: true,
       interim_results: true,
       endpointing: 500,
     });
+    this.finalResult = "";
     this.deepgramLive.addListener("transcriptReceived", (transcriptionMessage) => {
       const transcription = JSON.parse(transcriptionMessage);
       const text = transcription.channel?.alternatives[0]?.transcript;
-      if (transcription.is_final === true) {
-        if (text) {
-          this.emit("transcription", text);
+      console.log(transcription);
+      if (text) {
+        if (transcription.is_final === true && transcription.speech_final === false) {
+          this.finalResult += text;
+        } else if (transcription.is_final === true && transcription.speech_final === true) {
+          this.emit("transcription", this.finalResult);
+          this.finalResult = "";
+        } else {
+          this.emit("utterance", text);
         }
-      } else {
-        this.emit("utterance", text);
       }
     });
     this.deepgramLive.addListener("error", (error) => {
