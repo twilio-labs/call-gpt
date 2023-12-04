c97f672a5d68c9fa71378cb49fd63f17d30bde57
Charlie Weems <charlie.weems@gmail.com>
2023-12-03 20:35:33 -0800

    Extract GPT function to its own service

---
+++ gpt-service.js
@@ -0,0 +1,42 @@
+const EventEmitter = require("events");
+const OpenAI = require('openai');
+
+class GptService extends EventEmitter {
+  constructor() {
+    super();
+    this.openai = new OpenAI();
+    this.userContext = [
+      {"role": "system", "content": "You are a helpful assistant chatting with a user on the phone. Keep your responses cheerful and brief. If you receive a query that is not a complete sentence, respond with an empty string."},
+    ]
+  }
+
+  async completion(text) {
+    this.userContext.push({"role": "user", "content": text})
+
+    const stream = await this.openai.chat.completions.create({
+      model: "gpt-4",
+      messages: this.userContext,
+      stream: true,
+    });
+  
+    let completeResponse = ""
+    let partialResponse = ""
+
+    for await (const chunk of stream) {
+      let content = chunk.choices[0]?.delta?.content || ""
+      completeResponse += content;
+      if(content.slice(-1) === "." || content.slice(-1) === "!") {
+        console.log(partialResponse)
+        this.emit("gptreply", partialResponse);
+        partialResponse = ""
+      } else {
+        partialResponse += content 
+      }
+    }
+  
+    this.userContext.push({"role": "assistant", "content": completeResponse})
+  }
+
+}
+
+module.exports = { GptService }
--- server.js
+++ server.js
@@ -1,26 +1,22 @@
 require("dotenv").config();
 const express = require("express");
 const ExpressWs = require("express-ws");
-const OpenAI = require('openai');
 const uuid = require('uuid');
 
 const { TextToSpeechService } = require("./tts-service");
 const { TranscriptionService } = require("./transcription-service");
+const { GptService } = require("./gpt-service");
 
 const app = express();
 ExpressWs(app);
 
 const PORT = 3000;
-const openai = new OpenAI();
-
-const userContext = {}
 
 app.post("/incoming", (req, res) => {
   res.status(200);
   res.type("text/xml");
   res.end(`
   <Response>
-    <Play>https://maize-earwig-4391.twil.io/assets/ElevenLabs_2023-12-01T06_57_02_Rachel_pre_s50_sb75_se0_b_m2.mp3</Play>
     <Connect>
       <Stream url="wss://${process.env.SERVER}/connection" />
     </Connect>
@@ -28,47 +24,15 @@ app.post("/incoming", (req, res) => {
   `);
 });
 
-async function getGPTResponse(streamSid, text, ttsService) {
+// <Play>https://maize-earwig-4391.twil.io/assets/ElevenLabs_2023-12-01T06_57_02_Rachel_pre_s50_sb75_se0_b_m2.mp3</Play>
 
-  if (!userContext.hasOwnProperty(streamSid)) {
-      // Check for prior queries, create an array for them if one doesn't exist for the stram
-      userContext[streamSid] = [
-        {"role": "system", "content": "You are a helpful assistant chatting with a user on the phone. Keep your responses cheerful and brief. If you receive a query that is not a complete sentence, respond with an empty string."},
-      ];
-  }
-
-  userContext[streamSid].push({"role": "user", "content": text})
-
-
-  const stream = await openai.chat.completions.create({
-    model: "gpt-4",
-    messages: userContext[streamSid],
-    stream: true,
-  });
-
-  let completeResponse = ""
-  let partialResponse = ""
-  for await (const chunk of stream) {
-    let content = chunk.choices[0]?.delta?.content || ""
-    completeResponse += content;
-    if(content.slice(-1) === "." || content.slice(-1) === "!") {
-      console.log(partialResponse)
-      await ttsService.generate(partialResponse)
-      partialResponse = ""
-    } else {
-      partialResponse += content 
-    }
-  }
-
-  userContext[streamSid].push({"role": "assistant", "content": completeResponse})
-  completeResponse = ""
-}
 
 app.ws("/connection", (ws, req) => {
   ws.on("error", console.error);
   // Filled in from start message
   let streamSid;
 
+  const gptService = new GptService();
   const transcriptionService = new TranscriptionService();
   const ttsService = new TextToSpeechService({});
   let marks = []
@@ -88,6 +52,7 @@ app.ws("/connection", (ws, req) => {
   });
 
   transcriptionService.on("utterance", async (text) => {
+    // This is a bit of a hack to filter out empty utterances
     if(marks.length > 0 && text?.length > 5) {
       console.log("Interruption, Clearing stream")
       ws.send(
@@ -102,8 +67,13 @@ app.ws("/connection", (ws, req) => {
   transcriptionService.on("transcription", async (text) => {
     console.log(`Received final transcription: ${text}`);
     if (text) {
-      await getGPTResponse(streamSid, text, ttsService)
+      gptService.completion(text);
     }
+  });
+  
+  gptService.on('gptreply', async (text) => {
+    console.log('Sending GPT reply to TTS service...')
+    ttsService.generate(text);
   });
 
   ttsService.on("speech", (audio, label) => {
