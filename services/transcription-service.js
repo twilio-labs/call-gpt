5b86c135599fb3f57d63e329f5ab4cd0ac5c221a
Mark Vickstrom <mvickstrom@twilio.com>
2023-12-20 16:46:13 -0500

    added UtteranceEn
    added logic to use utteranceEnd message types from Deepgram. Massively improved detecting when speech ends in noisy environments.

--- services/transcription-service.js
+++ services/transcription-service.js
@@ -13,33 +13,51 @@ class TranscriptionService extends EventEmitter {
       punctuate: true,
       interim_results: true,
       endpointing: 200,
+      utterance_end_ms: 1000
     });
 
     this.finalResult = "";
+    this.speechFinal = false; // used to determine if we have seen speech_final=true indicating that deepgram detected a natural pause in the speakers speech. 
 
     this.deepgramLive.addListener("transcriptReceived", (transcriptionMessage) => {
       const transcription = JSON.parse(transcriptionMessage);
       const text = transcription.channel?.alternatives[0]?.transcript;
-      console.log(text, "is_final: ", transcription?.is_final, "speech_final: ", transcription.speech_final);
-      // if (transcription.is_final === true) {
-      //   this.finalResult += text;
-        if (transcription.speech_final === true || transcription.is_final === true) {
-          if (text.trim().length > 1) {
-            this.emit("transcription", text);
-            this.finalResult = "";
-          }
-          
+      
+      // if we receive an UtteranceEnd and speech_final has not already happened then we should consider this the end of of the human speech and emit the transcription
+      if (transcription.type === "UtteranceEnd") {
+        if (!this.speechFinal) {
+          console.log("UtteranceEnd received before speechFinal, emit the text collected so far: "+ this.finalResult)
+          this.emit("transcription", this.finalResult);
+          return;
+        } else {
+          console.log("speech was already final when UtteranceEnd recevied");
+          return;
         }
-      // } else {
-      //   this.emit("utterance", text);
-      // }
+      }
+
+      console.log(text, "is_final: ", transcription?.is_final, "speech_final: ", transcription.speech_final);
+      // if is_final that means that this chunk of the transcription is accurate and we need to add it to the finalResult 
+      if (transcription.is_final === true && text.trim().length > 0) {
+        this.finalResult += ` ${text}`;
+        // if speech_final and is_final that means this text is accurate and it's a natural pause in the speakers speech. We need to send this to the assistant for processing
+        if (transcription.speech_final === true) {
+          this.speechFinal = true; // this will prevent a utterance end which shows up after speechFinal from sending another response
+          this.emit("transcription", this.finalResult);
+          this.finalResult = "";
+        } else {
+          // if we receive a message without speechFinal reset speechFinal to false, this will allow any subsequent utteranceEnd messages to properly indicate the end of a message
+          this.speechFinal = false;
+        }
+      } else {
+        this.emit("utterance", text);
+      }
     });
 
     this.deepgramLive.addListener("error", (error) => {
       console.error("deepgram error");
       console.error(error);
     });
-    
+
     this.deepgramLive.addListener("close", () => {
       console.log("Deepgram connection closed");
     });
