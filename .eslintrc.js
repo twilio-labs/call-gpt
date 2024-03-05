a30279d4f34f6fb2877f24faa3e4f6d3b1380b32
Charlie Weems <charlie@wee.ms>
2024-03-04 23:57:16 -0800

    Lint app and replace fly.toml with example

---
+++ .eslintrc.js
@@ -0,0 +1,47 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'commonjs': true,
+    'es2021': true
+  },
+  'extends': 'eslint:recommended',
+  'overrides': [
+    {
+      'env': {
+        'node': true
+      },
+      'files': [
+        '.eslintrc.{js,cjs}'
+      ],
+      'parserOptions': {
+        'sourceType': 'script'
+      }
+    }
+  ],
+  'globals' : {
+    'expect': 'writeable',
+    'test': 'writeable',
+    'process': 'readable'
+  },
+  'parserOptions': {
+    'ecmaVersion': 'latest'
+  },
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ]
+  }
+};
