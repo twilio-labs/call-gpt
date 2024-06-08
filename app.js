847dffcc98340a4973a94cc1ec67b782e75b4cdb
Charlie Weems <charlie@wee.ms>
2024-06-08 10:10:15 -0700

    Upgrade to Deepgram v3 Node SDK

--- app.js
+++ app.js
@@ -26,74 +26,78 @@ app.post('/incoming', (req, res) => {
 });
 
 app.ws('/connection', (ws) => {
-  ws.on('error', console.error);
-  // Filled in from start message
-  let streamSid;
-  let callSid;
+  try {
+    ws.on('error', console.error);
+    // Filled in from start message
+    let streamSid;
+    let callSid;
 
-  const gptService = new GptService();
-  const streamService = new StreamService(ws);
-  const transcriptionService = new TranscriptionService();
-  const ttsService = new TextToSpeechService({});
+    const gptService = new GptService();
+    const streamService = new StreamService(ws);
+    const transcriptionService = new TranscriptionService();
+    const ttsService = new TextToSpeechService({});
   
-  let marks = [];
-  let interactionCount = 0;
-
-  // Incoming from MediaStream
-  ws.on('message', function message(data) {
-    const msg = JSON.parse(data);
-    if (msg.event === 'start') {
-      streamSid = msg.start.streamSid;
-      callSid = msg.start.callSid;
-      streamService.setStreamSid(streamSid);
-      gptService.setCallSid(callSid);
-      console.log(`Twilio -> Starting Media Stream for ${streamSid}`.underline.red);
-      ttsService.generate({partialResponseIndex: null, partialResponse: 'Hello! I understand you\'re looking for a pair of AirPods, is that correct?'}, 1);
-    } else if (msg.event === 'media') {
-      transcriptionService.send(msg.media.payload);
-    } else if (msg.event === 'mark') {
-      const label = msg.mark.name;
-      console.log(`Twilio -> Audio completed mark (${msg.sequenceNumber}): ${label}`.red);
-      marks = marks.filter(m => m !== msg.mark.name);
-    } else if (msg.event === 'stop') {
-      console.log(`Twilio -> Media stream ${streamSid} ended.`.underline.red);
-    }
-  });
-
-  transcriptionService.on('utterance', async (text) => {
-    // This is a bit of a hack to filter out empty utterances
-    if(marks.length > 0 && text?.length > 5) {
-      console.log('Twilio -> Interruption, Clearing stream'.red);
-      ws.send(
-        JSON.stringify({
-          streamSid,
-          event: 'clear',
-        })
-      );
-    }
-  });
-
-  transcriptionService.on('transcription', async (text) => {
-    if (!text) { return; }
-    console.log(`Interaction ${interactionCount} – STT -> GPT: ${text}`.yellow);
-    gptService.completion(text, interactionCount);
-    interactionCount += 1;
-  });
+    let marks = [];
+    let interactionCount = 0;
   
-  gptService.on('gptreply', async (gptReply, icount) => {
-    console.log(`Interaction ${icount}: GPT -> TTS: ${gptReply.partialResponse}`.green );
-    ttsService.generate(gptReply, icount);
-  });
-
-  ttsService.on('speech', (responseIndex, audio, label, icount) => {
-    console.log(`Interaction ${icount}: TTS -> TWILIO: ${label}`.blue);
-
-    streamService.buffer(responseIndex, audio);
-  });
-
-  streamService.on('audiosent', (markLabel) => {
-    marks.push(markLabel);
-  });
+    // Incoming from MediaStream
+    ws.on('message', function message(data) {
+      const msg = JSON.parse(data);
+      if (msg.event === 'start') {
+        streamSid = msg.start.streamSid;
+        callSid = msg.start.callSid;
+        streamService.setStreamSid(streamSid);
+        gptService.setCallSid(callSid);
+        console.log(`Twilio -> Starting Media Stream for ${streamSid}`.underline.red);
+        ttsService.generate({partialResponseIndex: null, partialResponse: 'Hello! I understand you\'re looking for a pair of AirPods, is that correct?'}, 1);
+      } else if (msg.event === 'media') {
+        transcriptionService.send(msg.media.payload);
+      } else if (msg.event === 'mark') {
+        const label = msg.mark.name;
+        console.log(`Twilio -> Audio completed mark (${msg.sequenceNumber}): ${label}`.red);
+        marks = marks.filter(m => m !== msg.mark.name);
+      } else if (msg.event === 'stop') {
+        console.log(`Twilio -> Media stream ${streamSid} ended.`.underline.red);
+      }
+    });
+  
+    transcriptionService.on('utterance', async (text) => {
+      // This is a bit of a hack to filter out empty utterances
+      if(marks.length > 0 && text?.length > 5) {
+        console.log('Twilio -> Interruption, Clearing stream'.red);
+        ws.send(
+          JSON.stringify({
+            streamSid,
+            event: 'clear',
+          })
+        );
+      }
+    });
+  
+    transcriptionService.on('transcription', async (text) => {
+      if (!text) { return; }
+      console.log(`Interaction ${interactionCount} – STT -> GPT: ${text}`.yellow);
+      gptService.completion(text, interactionCount);
+      interactionCount += 1;
+    });
+    
+    gptService.on('gptreply', async (gptReply, icount) => {
+      console.log(`Interaction ${icount}: GPT -> TTS: ${gptReply.partialResponse}`.green );
+      ttsService.generate(gptReply, icount);
+    });
+  
+    ttsService.on('speech', (responseIndex, audio, label, icount) => {
+      console.log(`Interaction ${icount}: TTS -> TWILIO: ${label}`.blue);
+  
+      streamService.buffer(responseIndex, audio);
+    });
+  
+    streamService.on('audiosent', (markLabel) => {
+      marks.push(markLabel);
+    });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 app.listen(PORT);
